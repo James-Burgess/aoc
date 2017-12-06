d64606b7f3a7ebbde0fb9fb45c0cfa18b7680feb
james-burgess <jms.bgr@gmail.com>
2017-12-06 12:31:06 +0200

    day threeee

---
+++ dayThree.py
@@ -0,0 +1,15 @@
+from math import sqrt
+
+def smallestCenterRoute(number):
+	row_number = (sqrt(number)+1)//2
+	bottom_num = (2*row_number-1)**2
+	top_num = (2*(row_number+1)-1)**2
+	row_len = (top_num-bottom_num)/4
+	center_dis = row_len/2
+
+	centers = [abs(number - (bottom_num + row_len*i +center_dis)) for i in range(4)]
+	smallest_distance = min(centers)+row_number
+	return(smallest_distance)
+
+number = 1024
+print(smallestCenterRoute(number))
