0643149dade1c064e1d2b0d270e6f784ae6b6ecc
james-burgess <jms.bgr@gmail.com>
2017-12-06 22:27:24 +0200

    day2part2 cleanup

--- d2/dayTwo.py
+++ d2/dayTwo.py
@@ -1,4 +1,7 @@
 def getInput():
+	#luss all this and do it in a listcomp
+ 	return([[int(a) for a in unsplit_array.split()] for unsplit_array in open('input.txt').read().split('\n')])
+
 # 	array = []
 # 	file = open('input.txt').read()
 # 	string_input = file.split('\n')
@@ -8,35 +11,30 @@ def getInput():
 # 		array.append(small_array)
 #	return(array)
 
-#luss all this and do it in a listcomp
- 	return([[int(a) for a in string.split()] for string in open('input.txt').read().split('\n')])
+
 
 def checksumCalc(array):
 	checksum = 0
 	for i in array:
 		checksum += max(i)-min(i)
-	print(checksum)
-
-def evenlyDivisibleValue(array):
-	checksum = 0
-	for i in range(len(array)):
-		for j in range(len(array[i])):
-			for k in range(j):
-				if array[i][j] % array[i][k] == 0: 
-					ans = array[i][j] / array[i][k]
-					checksum += ans
-					break
-				if array[i][k] % array[i][j] == 0:
-					ans = array[i][k] / array[i][j]
-					checksum += ans
-					break
 	return(checksum)
 
-
-
-array = getInput()
-checksumCalc(array)
+def evenlyDivisibleValue(large_array):
+	checksum = 0
+	for array in large_array:
+		for j in range(len(array)):
+			for k in range(j):
+				if array[j] % array[k] == 0: 
+					checksum += array[j] // array[k]; break
+				if array[k] % array[j] == 0: 
+					checksum += array[k] // array[j]; break
+	return(checksum)
 
 test_array = [[5,9,2,8],[9,4,7,3],[3,8,6,5]]
+array = getInput()
+
+print(checksumCalc(array))
+
+
 print(evenlyDivisibleValue(array))
 
