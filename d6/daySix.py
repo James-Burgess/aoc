efa5bf4caff7b495dbd85e28aaba2f46465afdf5
james-burgess <jms.bgr@gmail.com>
2018-02-18 06:55:15 +0200

    day 6

---
+++ d6/daySix.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""AOC Day 6.
+
+Author: James-Burgess (github.com/james-burgess)
+Licenese: free-to-do-what-you-please
+"""
+
+
+def main():
+	"""main function.
+    """
+    pass
+
+if __name__ == '__main__':
+    main()
